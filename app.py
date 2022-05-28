adbde698772fef5f9dfbdaddf9823aeac4044931
gianfranco-s <gianfranco.s@gmail.com>
2022-05-28 04:16:10 -0300

    Convert list elements to dictionaries where possible New way to send data to the template

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ app.config['CANTIDAD_DE_MESAS'] = int()
 
 mysql = MySQL()
 mysql.init_app(app)
-conn = mysql.connect()
-cursor = conn.cursor()
 
 database.create(mysql)
 database.create_admin_user(mysql, app.secret_key)
@@ -67,7 +65,19 @@ def ingresar():
 
 @app.route('/mesas/')
 def mesas():
-    """Listado de mesas, carga de pedidos por mesa, y cierre de mesa"""
+    """Listado de mesas, carga de pedidos por mesa, y cierre de mesa
+    mesas =[
+        {
+            'nro_mesa': int,
+            'pedido': {
+                        'plato': [...],
+                        'cantidad': [...],
+                        'subtotales': [...]
+                      },
+            'total': float
+        },
+    ]
+    """
 
     cookie = request.cookies.get('mesas')
     if cookie:
@@ -80,10 +90,16 @@ def mesas():
         mesas_backend = cursor.fetchall()
         mesas = list()
         for mesa in mesas_backend:
-            nro_de_mesa = mesa[0]
+            mesa_dict = dict()
+            pedido_dict = dict()
+
+            nro_mesa = mesa[0]
             pedido = mesa[1]
             suma = 0
-            subtotales = list()
+            
+            subtotales_list = list()
+            plato_list = list()
+            cantidad_list = list()
             if pedido:
                 pedido = json.loads(pedido)  # mesa[1] trae un json
                 for plato, cantidad in pedido.items():
@@ -91,21 +107,28 @@ def mesas():
                                    WHERE `nombre` LIKE %s;""", (plato))
                     precio_unitario = cursor.fetchone()[0]
                     subtotal_por_plato = precio_unitario * int(cantidad)
-                    subtotales.append((f'{plato}: {cantidad}',
-                                       subtotal_por_plato))
                     suma += subtotal_por_plato
+                    
+                    plato_list.append(plato)
+                    cantidad_list.append(cantidad)
+                    subtotales_list.append(subtotal_por_plato)
             else:
-                subtotales.append(['Sin pedidos', 0])
-            mesas.append([nro_de_mesa, subtotales, suma])
+                plato_list = ['Sin pedidos']
+                cantidad_list = ['']
+                subtotales_list = ['']
 
-        # Acotamos la cantidad de mesas al número indicado por el usuario
+            pedido_dict['plato'] = plato_list
+            pedido_dict['cantidad'] = cantidad_list
+            pedido_dict['subtotales'] = subtotales_list
+
+            mesa_dict['nro_mesa'] = nro_mesa
+            mesa_dict['pedido'] = pedido_dict
+            mesa_dict['total'] = suma
+            mesas.append(mesa_dict)
+
+        # Acotamos la cantidad de mesas al número indicado por el usuario en /administracion
         mesas = mesas[:app.config['CANTIDAD_DE_MESAS']]
-        # mesas: (list of lists)
-        # mesas[0]: (list)
-        #     mesas[0][0]: (int) número de mesa
-        #     mesas[0][1]: (tuple) pedido
-        #     mesas[0][1][0]: (str) item (agua, fideos, etc)
-        #     mesas[0][1][1]: (float) subtotal
+
         return render_template('/mesas.html', mesas=mesas)
     else:
         flash('Debe registrarse antes')
